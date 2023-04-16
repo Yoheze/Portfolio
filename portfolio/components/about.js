7f19c35bcfd8c4b9b52307a9608d1706f5b89ba9
Yohan <yohanjeon1@gmail.com>
2023-04-15 17:53:01 -0700

    vercel deployment attempt 2

--- portfolio/components/about.js
+++ portfolio/components/about.js
@@ -2,28 +2,28 @@ import styles from '@/styles/About.module.css'
 import React from 'react';
 import Image from 'next/image';
 import { Link } from 'react-scroll'
-import Self from '../public/me.jpg'
-import Codesmith from '../public/codesmith.png'
-import UCR_Logo from '../public/ucr-logo.png'
-import HTML_Logo from '../public/html-logo.png';
-import CSS_Logo from '../public/css-logo.png';
-import JS_Logo from '../public/Javascript-logo.png';
-import React_Logo from '../public/react-icon.png';
-import Express_Logo from '../public/express-logo.png';
-import MongoDB_Logo from '../public/mongodb-logo.png';
-import Redux_Logo from '../public/redux-logo.png';
-import TS_Logo from '../public/typescript-logo.png';
-import Git_Logo from '../public/git-logo.png';
-import GitHub_Logo from '../public/github-logo.jpg';
-import Websocket_Logo from '../public/websocket-logo.png';
-import OAuth_Logo from '../public/oauth2-logo.png';
-import Node_Logo from '../public/node-logo.png';
-import PostgreSQL_Logo from '../public/postgresql-logo.png';
-import Webpack_Logo from '../public/webpack-logo.png';
-import Tailwind_Logo from '../public/tailwind-logo.png';
-import React_Router_Logo from '../public/react-router-logo.png';
-import Vite_Logo from '../public/vite-logo.png';
-import Next_Logo from '../public/next.svg'
+import Self from '@/public/me.jpg'
+import Codesmith from '@/public/codesmith.png'
+import UCR_Logo from '@/public/ucr-logo.png'
+import HTML_Logo from '@/public/html-logo.png';
+import CSS_Logo from '@/public/css-logo.png';
+import JS_Logo from '@/public/Javascript-logo.png';
+import React_Logo from '@/public/react-icon.png';
+import Express_Logo from '@/public/express-logo.png';
+import MongoDB_Logo from '@/public/mongodb-logo.png';
+import Redux_Logo from '@/public/redux-logo.png';
+import TS_Logo from '@/public/typescript-logo.png';
+import Git_Logo from '@/public/git-logo.png';
+import GitHub_Logo from '@/public/github-logo.jpg';
+import Websocket_Logo from '@/public/websocket-logo.png';
+import OAuth_Logo from '@/public/oauth2-logo.png';
+import Node_Logo from '@/public/node-logo.png';
+import PostgreSQL_Logo from '@/public/postgresql-logo.png';
+import Webpack_Logo from '@/public/webpack-logo.png';
+import Tailwind_Logo from '@/public/tailwind-logo.png';
+import React_Router_Logo from '@/public/react-router-logo.png';
+import Vite_Logo from '@/public/vite-logo.png';
+import Next_Logo from '@/public/next.svg'
 
 export default function About () {
 
