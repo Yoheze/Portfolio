9286b2c3e5c0026c352b638b3a6bb350a3dd799b
Yohan <yohanjeon1@gmail.com>
2023-04-17 13:28:17 -0700

    mobile attempt four

--- portfolio/pages/_document.js
+++ portfolio/pages/_document.js
@@ -1,19 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,31 +1,33 @@
-import NavBar from '@/components/navbar.js'
-import Header from '@/components/header.js'
-import About from '@/components/about.js'
-import Experience from '@/components/experience.js'
-import Projects from '@/components/projects.js'
-import Contact from '@/components/contact.js'
-import Footer from '@/components/footer.js'
-import Head from 'next/head'
+import NavBar from '@/components/navbar.js';
+import Header from '@/components/header.js';
+import About from '@/components/about.js';
+import Experience from '@/components/experience.js';
+import Projects from '@/components/projects.js';
+import Contact from '@/components/contact.js';
+import Footer from '@/components/footer.js';
+import Head from 'next/head';
 
 export default function Home() {
-
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Yohan&apos;s Portfolio</title>
       </Head>
-      <NavBar/>
-      <Header/>
-      <About/>
-      <Experience/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+      <NavBar />
+      <Header />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
+
