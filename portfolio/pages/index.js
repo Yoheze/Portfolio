1bf1b46210fc5b39b770a1ce0804a4e1aef85fc7
Yohan <yohanjeon1@gmail.com>
2023-06-10 18:20:25 -0700

    small additions and fixes

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/react-logo.png" type="image/png"></link>
         <title>Yohan&apos;s Portfolio</title>
       </Head>
       <NavBar />
